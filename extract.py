3c75d084cc3977fa3375c31fd53206cc131bb509
Harry Lee <leeharry92@gmail.com>
2021-10-14 19:16:12 -0400

    new implementation for gpu option

--- extract.py
+++ extract.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
 import argparse
 import pathlib
 import logging
@@ -75,31 +76,13 @@ def create_parser():
     return parser
 
 
-def main(args):
-    # Set up logging
-    logging_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging_level = logging.DEBUG
-    logging_encoding = 'utf-8'
-    if args.log_file is not None:
-        logging.basicConfig(filename=args.log_file, encoding=logging_encoding,
-                            level=logging_level, format=logging_format)
-    else:
-        logging.basicConfig(stream=stdout, encoding=logging_encoding,
-                            level=logging_level, format=logging_format)
-
-
+def main(args, gpu_id):
     model, alphabet = pretrained.load_model_and_alphabet(args.model_location)
     model.eval()
-    if torch.cuda.is_available() and not args.nogpu:
-        model = model.cuda()
-        logging.info("Transferred model to GPU")
 
-        if args.gpu_id is None:
-            logging.warning(f"The id for the GPU to compute the embeddings was not specified. Defaulting to {DEFAULT_GPU}")
-            gpu_id = DEFAULT_GPU
-        else:
-            logging.info(f"Computing embeddings on GPU {args.gpu_id}")
-            gpu_id = args.gpu_id
+    if torch.cuda.is_available() and not args.nogpu:
+        model = model.to(torch.device(f'cuda:{gpu_id}'))
+        logging.info("Transferred model to GPU")
 
     dataset = FastaBatchedDataset.from_file(args.fasta_file)
     batches = dataset.get_batch_indices(args.toks_per_batch, extra_toks_per_seq=1)
@@ -165,8 +148,35 @@ def main(args):
                 )
 
 
+def setup(args):
+    # Set up logging
+    logging_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging_level = logging.DEBUG
+    logging_encoding = 'utf-8'
+    if args.log_file is not None:
+        logging.basicConfig(filename=args.log_file, encoding=logging_encoding,
+                            level=logging_level, format=logging_format)
+    else:
+        logging.basicConfig(stream=stdout, encoding=logging_encoding,
+                            level=logging_level, format=logging_format)
+
+    # Set up GPU
+    if args.gpu_id is None:
+        logging.warning(f"The id for the GPU to compute the embeddings was not specified. Defaulting to {DEFAULT_GPU}")
+        gpu_id = DEFAULT_GPU
+    else:
+        logging.info(f"Computing embeddings on GPU {args.gpu_id}")
+        gpu_id = args.gpu_id
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
+
+    return gpu_id
+
+
 if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
-    main(args)
+    gpu_id = setup(args)
+    main(args, gpu_id)
     logging.info('Successfully completed')
